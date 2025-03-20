a1f04758609bb3a6886d6c3207a0d6937cbdcfb1
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-03-20 08:03:21 -0400

    Update obj_with_type.py
    dunder methods for print and interative python shell.

--- obj_with_type.py
+++ obj_with_type.py
@@ -1,8 +1,13 @@
 # build a class with type()
-Obj = type("Obj", (object, ), dict(add=(lambda self, x, y: x+y), 
+Obj = type("Obj", (object, ), dict(indice=0,
+                                   add=(lambda self, x, y: x+y), 
                                    times=(lambda self, x, y: x*y), 
-                                   exp=(lambda self, x, y: x**y))
+                                   exp=(lambda self, x, y: x**y),
+                                  __str__=(lambda self:f"<{self.__class__.__name__} n°{self.indice}>"),
+                                  __repr__=(lambda self:f"<{self.__class__.__name__} -> (atributos aqui)>"))
           )
 obj = Obj()
+objn = Obj()
+objn.indice = 17
 print(f"Nome do objeto: --- {obj.__class__.__name__} ---\nTipo: {type(obj)}\nInstancia:\t{isinstance(obj, object)}")
 print("Soma:%s\nMultiplicação:%s\nExponenciação:%s" % (obj.add(22.5, 12.5), obj.times(125, 25), obj.exp(2, 10)))
