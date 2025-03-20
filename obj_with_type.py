e8762a9a08650d13dfa61da738fd386f483624ad
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-03-19 23:36:16 -0400

    Update obj_with_type.py
    Mais modificações.

--- obj_with_type.py
+++ obj_with_type.py
@@ -1,4 +1,5 @@
 # build a class with type()
 Obj = type("Obj", (object, ), dict(add=(lambda self, x, y: x+y), times=(lambda self, x, y: x*y), exp=(lambda self, x, y: x**y)))
 obj = Obj()
+print(f"Nome do objeto: --- {obj.__class__.__name__} ---\nTipo: {type(obj)}\n Instancia:{isinstance(obj, type)}")
 print("Soma:%s\nMultiplicação:%s\nExponenciação:%s" % (obj.add(22.5, 12.5), obj.times(125, 25), obj.exp(2, 10)))
